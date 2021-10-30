3f72232aadc2201b47a7a6b5af317e4d1f287ca5
MosheL <Moshe.Laufer@rada.com>
2021-10-30 23:19:54 +0300

    ver0

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    my_list = []
+    for i in range(10):
+        my_list.append(i)
+    if len(my_list) > 0:
+        print(my_list)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ moodle_example.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+import datargs
+
+
+@dataclass
+class MyArgs:
+    """
+    When using code from the Command Line Interface (CLI), underscores (_) should be replaced by dashes (-).
+    For example, to run this file with different arguments use the command
+    python cmd_example.py --num-people 10 --num-simulations 3
+    The usage example looks like this:
+    D:\Downloads>python cmd_example.py --num-people 10 --num-simulations 3
+    Running 3 simulations with 10 people each.
+    Note that every variable must have a type, otherwise you won't be able to change this argument from the shell
+    """
+    num_people: int = int(1e5)  # Comments can be used for documentation
+    num_simulations: int = 100  # Unlike argparse's help, comments won't be presented in the CLI
+
+
+def main(args: MyArgs):
+    print(f"Running {args.num_simulations} simulations with {args.num_people} people each.")
+
+
+if __name__ == "__main__":
+    args = datargs.parse(MyArgs)
+    main(args)
